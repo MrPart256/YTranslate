16b43e0924a4043c524715a6e1865eb4122fc3b7
MrPart256 <662212>
2020-03-13 20:54:01 +0700

    Initial commit

---
+++ YandexTranslate/Models/Translate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexLinguistics.NET;
+
+namespace YandexTranslate
+{
+    class TTranslate
+    {
+        YandexLinguistics.NET.Translator translator;
+        const string translatorKey = ""; //put your api key here
+        public TTranslate()
+        {
+            translator = new Translator(translatorKey);
+            
+        }
+        public LangPair GetLangPair(string inputLang,string outputLang)
+        {
+            LangPair lp = new LangPair();
+            #region InputLang
+
+            switch (inputLang)
+            {
+                case "Английский":
+                    lp.InputLang = Lang.En;
+                    break;
+                case "Русский":
+                    lp.InputLang = Lang.Ru;
+                    break;
+                case "Немецкий":
+                    lp.InputLang = Lang.De;
+                    break;
+            }
+           
+            #endregion
+            #region OutputLang
+            switch (outputLang)
+            {
+                case "Английский":
+                   
+                    lp.OutputLang = Lang.En;
+                    break;
+                case "Немецкий":
+                    lp.OutputLang = Lang.De;
+                    break;
+                case "Русский":
+                    lp.OutputLang = Lang.Ru;
+                    break;
+            }
+            
+            #endregion
+            return lp;
+            }
+        public  string Translator(string wordToTranslate,LangPair langPair)
+        {
+            return translator.Translate(wordToTranslate, langPair).Text;
+         
+        }
+    }
+}
---
+++ YandexTranslate/ViewModels/MainViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using YandexTranslate.Commands;
+
+namespace YandexTranslate.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+   
+        private string _inputString;
+
+        public string InputString
+        {
+            get
+            {
+                return _inputString;
+            }
+            set
+            {
+                _inputString = value;
+                OnPropertyChanged(nameof(InputString));
+            }
+        }
+        private string _OutputString;
+
+        public string OutputString
+        {
+            get
+            {
+                return _OutputString;
+            }
+            set
+            {
+                _OutputString = value;
+                OnPropertyChanged(nameof(OutputString));
+            }
+        }
+
+        private string _selectedInputLang;
+
+        public string SelectedInputLang
+        {
+            get
+            {
+                return _selectedInputLang;
+            }
+            set
+            {
+                _selectedInputLang = value;
+                OnPropertyChanged(nameof(SelectedInputLang));
+            }
+        }
+        public List<string> InputLang
+        {
+            get
+            {
+                return new List<string>
+                {
+                    "Английский",
+                    "Русский",
+                    "Немецкий"
+                };
+            }
+        }
+        private string _selectedOutputLang;
+        public string SelectedOutputLang
+        {
+            get
+            {
+                return _selectedOutputLang;
+            }
+            set
+            {
+                _selectedOutputLang = value;
+                OnPropertyChanged(nameof(SelectedOutputLang));
+            }
+        }
+        public List<string> OutputLang
+        {
+            get
+            {
+                return new List<string>
+                {
+                    "Английский",
+                    "Русский",
+                    "Немецкий"
+                };
+            }
+        }
+
+        private ICommand command;
+
+        public ICommand Command
+        {
+            get
+            {
+                if (command == null)
+                {
+                    command = new DelegateCommand(() => { Translate(); }, () => !string.IsNullOrEmpty(_inputString));
+                }
+                return command;
+            }
+
+        }
+        private void Translate()
+        {
+            TTranslate ttr = new TTranslate();
+            OutputString = ttr.Translator(_inputString, ttr.GetLangPair(SelectedInputLang, SelectedOutputLang));
+        }
+
+    }
+}
